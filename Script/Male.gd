9a84ddfbeda4184ddb05f38c4ea37027549c9326
27794 <2779435956@qq.com>
2023-03-27 17:06:28 +0800

    doc[doc]:人物移动动画

---
+++ Script/Male.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+#重力
+const gravity = 2000
+#速度
+const speed = 500
+#跳越的高度
+const jump_force = 780
+#角色的向量
+var velocity = Vector2.ZERO
+#角色是否在跳越
+var is_on_jumping = false
+
+
+onready var AnimatedSprite = $AnimatedSprite
+
+
+#物理帧，处理移动
+func _physics_process(_delta):
+	velocity = move_and_slide(velocity,Vector2.UP)
+	if is_on_floor():
+		is_on_jumping = false
+		
+#输入函数		
+func _input(_event):
+	if Input.is_action_pressed("jump") and not is_on_jumping:
+		velocity.y = -jump_force
+		is_on_jumping = true
+
+
+
+func _process(delta):
+	var direction = Input.get_action_strength("move_left")-Input.get_action_strength("move_righ")
+	velocity.x = -direction*speed
+	velocity.y += gravity*delta
+	if is_on_jumping:
+		AnimatedSprite.play("jump")
+	elif direction == 0:
+		AnimatedSprite.play("idle")
+		
+	else:
+		AnimatedSprite.play("walk")
+		#水平
+		AnimatedSprite.flip_h  = direction>0
