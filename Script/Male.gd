38a5dd1ce4b171fef7d8d58ff34e14188c7d13d9
27794 <2779435956@qq.com>
2023-03-29 22:16:17 +0800

    doc[doc]:实现郊狼时间和摩檫力

--- Script/Male.gd
+++ Script/Male.gd
@@ -1,8 +1,12 @@
 extends KinematicBody2D
 #重力
 const gravity = 2300
-#速度
-const speed = 550
+#最大速度
+const max_speed = 678
+#在地面的摩擦力
+const acceleration = max_speed/0.22
+#角色在空中的摩檫力
+const air_acceleration = max_speed/0.06
 #跳越的高度
 const jump_force = 820
 #角色的向量
@@ -12,30 +16,52 @@ var is_on_jumping = false
 
 
 onready var AnimatedSprite = $AnimatedSprite
+onready var coyot_time = $Timer
+onready var jump_request_time = $Timer2
 
 
 #物理帧，处理移动
 func _physics_process(_delta):
+	var was_on_floor = is_on_floor()
 	velocity = move_and_slide(velocity,Vector2.UP)
 	if is_on_floor():
 		is_on_jumping = false
+	#没在地面并没有按下跳越键触发郊狼时间
+	elif was_on_floor and not is_on_jumping:
+		coyot_time.start()
+
 		
 #输入函数		
 func _input(_event):
-	if Input.is_action_pressed("jump") and not is_on_jumping:
+	
+	var can_jump = is_on_floor() or coyot_time.time_left>0
+	if Input.is_action_just_pressed("jump"):
+		jump_request_time.start()
+	if Input.is_action_pressed("jump") and can_jump:
 		velocity.y = -jump_force
 		is_on_jumping = true
-
+		jump_request_time.stop()
+		coyot_time.stop()
+	#释放按键的时间决定跳越的高度
+	if Input.is_action_just_released("jump") and velocity.y < -jump_force/1.7:
+		velocity.y = -jump_force/1.7
 
 
 func _process(delta):
+	#角色的移动向量
 	var direction = Input.get_action_strength("move_left")-Input.get_action_strength("move_righ")
-	velocity.x = -direction*speed
+	#空中与地面的摩檫力差别
+	if is_on_floor():
+		var acc = acceleration
+	else:
+		var acc = air_acceleration
+	#使角色有摩檫力
+	velocity.x = move_toward(velocity.x,-direction*max_speed,acceleration*delta)
 	velocity.y += gravity*delta
 	#动画播放
 	if is_on_jumping:
 		AnimatedSprite.play("jump")
-	elif direction == 0:
+	elif velocity.x==0:
 		AnimatedSprite.play("idle")
 	else:
 		AnimatedSprite.play("walk")
