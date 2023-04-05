a575de3cb1ff0a9202cec08cd3136645e68f3572
27794 <2779435956@qq.com>
2023-04-05 15:04:39 +0800

    doc[doc]:按钮功能实现

--- Script/Globals.gd
+++ Script/Globals.gd
@@ -1,10 +1,29 @@
 extends Node2D
+#总线变量
+const BGM_ID = 1
+const SFX_ID = 2
+var Bgm_enable = true
+var Sfx_enable = true
+
 
 onready var animation_player = $AnimationPlayer
 
+#切换世界动画
 func go_to_world(path):
 	animation_player.play_backwards("fade_in")
 	yield(animation_player,"animation_finished")
 	get_tree().change_scene(path)
 	animation_player.play("fade_in")
 	pass
+#读取音乐状态	
+func is_bgm_enabled():
+	return not AudioServer.is_bus_mute(BGM_ID)
+#设置音乐状态	
+func set_bgm_enable():
+	AudioServer.set_bus_mute(BGM_ID,!AudioServer.is_bus_mute(BGM_ID))
+#读取音效状态	
+func is_sfx_enabled():
+	return not AudioServer.is_bus_mute(SFX_ID)
+#设置音效状态	
+func set_sfx_enable():
+	AudioServer.set_bus_mute(SFX_ID,!AudioServer.is_bus_mute(SFX_ID))
--- Script/Title.gd
+++ Script/Title.gd
@@ -1,20 +1,61 @@
 extends Control
 #预加载
-onready var button_2 = $VBoxContainer/Button2
 onready var title_animation = $Title_animation
 onready var parallax_background = $ParallaxBackground
-#
+
+onready var yes_audio = $Yes_audio
+onready var no_audio = $No_audio
+onready var go_audio = $Go_audio
+
+onready var music = $VBoxContainer2/Music
+onready var sfx = $VBoxContainer2/Sfx
+
+
 export var scroll_velocity = 55
 
 func _process(delta):
 	parallax_background.scroll_offset.x += scroll_velocity*delta
+
+
 	
 #设置按钮
 func _on_Button2_pressed():
+	yes_audio.play(0)
 	title_animation.play("button_in")
 	pass # Replace with function body.
 	
 #返回按钮
 func _on_Button3_pressed():
+	no_audio.play(0)
 	title_animation.play_backwards("button_in")
 	pass # Replace with function body.
+
+#开始按钮
+func _on_Button_pressed():
+	go_audio.play(0)
+	yield(go_audio,"finished")
+	Globals.go_to_world("res://Scene/World.tscn")
+	pass # Replace with function body.
+
+#退出按钮
+func _on_Quit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+#音乐开关
+func _on_Music_pressed():
+	
+	Globals.set_bgm_enable()
+	update_button()
+	pass # Replace with function body.
+
+#音效开关
+func _on_Sfx_pressed():
+	Globals.set_sfx_enable()
+	update_button()
+	pass # Replace with function body.
+
+func update_button():
+	music.text = "音乐:"+("开" if Globals.is_bgm_enabled() else "关")
+	sfx.text = "音效:"+("开" if Globals.is_sfx_enabled() else "关")
+
